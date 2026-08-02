fc522f25d9d3c6ddede343df4d327016bf098bb7
Peter Komár <peterkomar1337@gmail.com>
2026-08-02 20:06:13 +0200

    feat/DonationAmount component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {Stepper} from "@/components/Stepper/Stepper";
 import styles from "./page.module.scss";
 import {DonationChoice} from "@/components/DonationChoice/DonationChoice";
 import {ShelterSelect} from "@/components/ShelterSelect/ShelterSelect";
+import DonationAmount from "@/components/DonationAmount/DonationAmount";
 
 export default function Home() {
 
@@ -26,37 +27,7 @@ export default function Home() {
 							<ShelterSelect/>
 						</div>
 
-						{/*todo: component*/}
-						<div className="donationWrapper">
-							<label htmlFor="donation">Suma, ktorou chcete zaplatiť</label>
-							<input type="number" id="donation" name="donation" />
-							<div className="donationButtonList">
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount1" value="5" />
-									<label htmlFor="amount1">5 €</label>
-								</div>
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount2" value="10" />
-									<label htmlFor="amount2">10 €</label>
-								</div>
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount3" value="20" />
-									<label htmlFor="amount3">20 €</label>
-								</div>
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount4" value="30" />
-									<label htmlFor="amount4">30 €</label>
-								</div>
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount5" value="50" />
-									<label htmlFor="amount5">50 €</label>
-								</div>
-								<div className="donationPreset">
-									<input type="radio" name="amount" id="amount6" value="100" />
-									<label htmlFor="amount6">100 €</label>
-								</div>
-							</div>
-						</div>
+						<DonationAmount/>
 
 						{/*todo: component*/}
 						<div className="stepNavigation">
---
+++ src/components/DonationAmount/DonationAmount.tsx
@@ -0,0 +1,41 @@
+import styles from './DonationAmount.module.scss';
+const PRESETS = [5, 10, 20, 30, 50, 100];
+
+export default function DonationAmount() {
+	return (
+		<fieldset className={styles.wrapper}>
+			<legend className={styles.legend}>Suma, ktorou chcem prispieť</legend>
+
+			<div className={styles.custom}>
+				<label className="sr-only" htmlFor="amount">Vlastná suma v eurách</label>
+				<input
+					className={styles.input}
+					type="number"
+					id="amount"
+					name="amount"
+					placeholder="0"
+					min="1"
+					inputMode="numeric"
+				/>
+				<span className={styles.currency} aria-hidden>€</span>
+			</div>
+
+			<div className={styles.presets}>
+				{PRESETS.map((value) => (
+					<div className={styles.preset} key={value}>
+						<input
+							className={`sr-only ${styles.presetInput}`}
+							type="radio"
+							name="amountPreset"
+							id={`amount-${value}`}
+							value={value}
+						/>
+						<label className={styles.presetLabel} htmlFor={`amount-${value}`}>
+							{value} €
+						</label>
+					</div>
+				))}
+			</div>
+		</fieldset>
+	)
+}
